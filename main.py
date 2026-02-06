b466a05616d3f4e1cea53a7ae89397738092cac2
jhill281 <jameshillre123@gmail.com>
2026-02-06 05:16:08 -0500

    Implement game manager and main function (INCOMPLETE)
    Added game management functionality and main execution flow.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#Import modules
+from game_manager import GameManager
+
+def main():
+    game = GameManager(player_names = None)
+
+    print("Solution cards (for testing): ", game.card_manager.solution)
+
+    game.run_game_loop()
+
+    print("\nGame Over!")
+
+if __name__ == "__main__":
+    main()
+    
