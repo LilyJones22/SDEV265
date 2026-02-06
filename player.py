7877ecedbf36dcf47652d312e6e8922cd89b5fb2
jhill281 <jameshillre123@gmail.com>
2026-02-06 03:02:14 -0500

    Define Player class with initialization
    Added a Player class with initialization for name, position, hand, and elimination status.

--- player.py
+++ player.py
@@ -1,1 +1,7 @@
-
+# Player Class Definition
+class Player:
+    def __init__(self, name):           # Initialization Function
+        self.name = name                # Player name
+        self.position = 0               # Position on board
+        self.hand = []                  # List of cards in hand
+        self.is_eliminated = False      # Elimination status
