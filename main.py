295593052acd0a43e5bade5fd68deb841a20fd83
jhill281 <jameshillre123@gmail.com>
2026-02-09 05:07:09 -0500

    Add print statement for testing solution cards
    Added print statement to display solution cards for testing.

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from game_manager import GameManager
 def main():
     game = GameManager(player_names = None)
 
+    # Print randomly chosen solution for testing purposes
     print("Solution cards (for testing): ", game.card_manager.solution)
 
+    # Runs game loop function from game_manager
     game.run_game_loop()
 
+    # Prints Game Over when game loop ends
     print("\nGame Over!")
 
-if __name__ == "__main__":
+if __name__ == "__main__":      # Only runs main() function if this file is executed directly
     main()
     
