6ec9e1a5d5986e7339c7c6783c494a5fa30900c3
jhill281 <jameshillre123@gmail.com>
2026-02-20 01:59:09 -0500

    Integrate ClueUI for game interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Import modules
 from game_manager import GameManager
+from UI import ClueUI
 
 def main():
     game = GameManager(player_names = None)
@@ -7,12 +8,14 @@ def main():
     # Print randomly chosen solution for testing purposes
     print("Solution cards (for testing): ", game.card_manager.solution)
 
+    # Generate GUI
+    app = ClueUI(game)
+
     # Runs game loop function from game_manager
-    game.run_game_loop()
+    app.mainloop()
 
     # Prints Game Over when game loop ends
     print("\nGame Over!")
 
 if __name__ == "__main__":      # Only runs main() function if this file is executed directly
     main()
-    
